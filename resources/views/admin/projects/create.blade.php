346ca27e4e15780908c9a3740566b7823322d253
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-06 17:08:43 +0100

    errors added

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -3,6 +3,21 @@
 	<div class="container">
 		<div class="row">
 			<div class="col py-5">
+
+				{{-- Errori --}}
+				@if ($errors->any())
+					<div class="errors">
+						<div class="alert alert-danger">
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					</div>
+				@endif
+
+				{{-- Form di creazione --}}
 				<div class="d-flex justify-content-between mb-3">
 					<h3 class="fw-bold">CREA UN NUOVO PROGETTO</h3>
 					<a href="{{ route('admin.projects.index') }}"><button class="btn btn-secondary"><i class="fa-solid fa-arrow-left me-2"></i>Torna alla lista progetti</button></a>
@@ -13,10 +28,22 @@
 						<label for="" class="form-label">Titolo</label>
 						<input type="text" id="" aria-describedby="" name="title" class="form-control" placeholder="Aggiungi titolo">
 					</div>
+
 					<div class="mb-4">
 						<label for="" class="form-label">Descrizione</label>
 						<textarea rows="5" id="" aria-describedby="" name="content" class="form-control" placeholder="Aggiungi descrizione"></textarea>
-					</div>								
+					</div>	
+
+					<div class="mb-4">
+						<label for="" class="form-label">Linguaggio</label>
+						<select name="type_id" id="type_id" class="form-control">
+							<option value="">Seleziona linguaggio</option>
+							@foreach ($types as $item)
+								<option value="{{ $item->id }}">{{ $item->name }}</option>
+							@endforeach
+						</select>
+					</div>	
+
 					<div class="form-group">
 						<button type="submit" class="btn btn-primary">Crea progetto</button>
 					</div>
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -3,6 +3,21 @@
 	<div class="container">
 		<div class="row">
 			<div class="col py-5">
+
+				{{-- Errori --}}
+				@if ($errors->any())
+					<div class="errors">
+						<div class="alert alert-danger">
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					</div>
+				@endif
+
+				{{-- Form di modifica --}}
 				<div class="d-flex justify-content-between mb-3">
 					<h3 class="fw-bold">MODIFICA PROGETTO</h3>
 					<a href="{{ route('admin.projects.index') }}"><button class="btn btn-secondary"><i class="fa-solid fa-arrow-left me-2"></i>Torna alla lista progetti</button></a>
@@ -14,10 +29,22 @@
 						<label for="" class="form-label">Titolo</label>
 						<input type="text" id="" aria-describedby="" value="{{ old('title') ?? $project->title }}" name="title" class="form-control" placeholder="Aggiungi titolo">
 					</div>
+
 					<div class="mb-4">
 						<label for="" class="form-label">Descrizione</label>
 						<textarea rows="5" id="" aria-describedby="" name="content" class="form-control" placeholder="Aggiungi descrizione">{{ old('title') ?? $project->content }}</textarea>
-					</div>								
+					</div>
+
+					<div class="mb-4">
+						<label for="" class="form-label">Linguaggio</label>
+						<select name="type_id" id="type_id" class="form-control">
+							<option value="">Seleziona linguaggio</option>
+							@foreach ($types as $item)
+								<option value="{{ $item->id }}" {{ $item->id == old('type_id', $project->type_id) ? 'selected' : '' }}>{{ $item->name }}</option>
+							@endforeach
+						</select>
+					</div>	
+
 					<div class="form-group">
 						<button type="submit" class="btn btn-primary">Salva modifiche</button>
 					</div>
